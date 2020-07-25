d67730074f023534b2282d92aa41f1954a15bd0d
Milan Winter <milanwinter8@gmail.com>
2020-07-25 23:58:44 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -9,10 +9,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-date = generate_star_date
-
 def engage
   puts state_log(date)
+  date = generate_star_date
 end
-binding.pry
-puts "hello"
